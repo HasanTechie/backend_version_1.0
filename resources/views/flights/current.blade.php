b58318b5fb89d2c0492387717bc0240d1fed8cda
Hasan <hasanabbax@gmail.com>
2019-01-03 04:47:52 +0000

    minor fixes

--- resources/views/flights/current.blade.php
+++ resources/views/flights/current.blade.php
@@ -12,10 +12,10 @@
             <th scope="col">IATA flight_number</th>
             <th scope="col">Airline</th>
             <th scope="col">Arrival Airport Scheduled</th>
-            <th scope="col">Arrival Runwaytime Initial</th>
+            {{--<th scope="col">Arrival Runwaytime Initial</th>--}}
             <th scope="col">Arrival Runwaytime Estimated</th>
             <th scope="col">Departure Airport Scheduled</th>
-            <th scope="col">Departure Runwaytime Initial</th>
+            {{--<th scope="col">Departure Runwaytime Initial</th>--}}
             <th scope="col">Flight Status</th>
         </tr>
         </thead>
@@ -27,10 +27,10 @@
                 <td>{{$value->properties->iataFlightNumber}}</td>
                 <td>{{$value->properties->airline}}</td>
                 <td>{{$value->properties->arrival->aerodrome->scheduled}}</td>
-                <td>{{$value->properties->arrival->runwayTime->initial}}</td>
+{{--                <td>{{$value->properties->arrival->runwayTime->initial}}</td>--}}
                 <td>{{$value->properties->arrival->runwayTime->estimated}}</td>
                 <td>{{$value->properties->departure->aerodrome->scheduled}}</td>
-                <td>{{$value->properties->departure->runwayTime->initial}}</td>
+{{--                <td>{{$value->properties->departure->runwayTime->initial}}</td>--}}
                 <td>{{$value->properties->flightStatus}}</td>
             </tr>
         @endforeach

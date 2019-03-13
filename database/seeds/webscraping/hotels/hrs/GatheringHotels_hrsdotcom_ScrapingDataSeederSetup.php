91cbf6a1d3cab79cd230b6efde5e1c797a11f8bc
hasanabbax <hasanabbax@gmail.com>
2019-03-13 09:34:12 +0000

    updated Main Seeder of HRS

--- database/seeds/webscraping/hotels/hrs/GatheringHotels_hrsdotcom_ScrapingDataSeederSetup.php
+++ database/seeds/webscraping/hotels/hrs/GatheringHotels_hrsdotcom_ScrapingDataSeederSetup.php
@@ -13,14 +13,14 @@ class GatheringHotels_hrsdotcom_ScrapingDataSeederSetup extends Seeder
     {
         $hrs = new GatheringHotels_hrsdotcom_ScrapingDataSeederMain();
 
-        $dataArray [] = [
-            'currency' => 'EUR',
-            'start_date' => '2019-03-20',
-            'end_date' => '2020-03-20',
-            'city' => 'Rome',
-            'city_id' => 54084,
-            'country_code' => 'IT',
-        ];
+//        $dataArray [] = [
+//            'currency' => 'EUR',
+//            'start_date' => '2019-03-20',
+//            'end_date' => '2020-03-20',
+//            'city' => 'Rome',
+//            'city_id' => 54084,
+//            'country_code' => 'IT',
+//        ];
 //        $dataArray [] = [
 //            'currency' => 'EUR',
 //            'start_date' => '2019-03-14',
@@ -37,14 +37,14 @@ class GatheringHotels_hrsdotcom_ScrapingDataSeederSetup extends Seeder
 //            'city_id' => 91191,
 //            'country_code' => 'DE',
 //        ];
-//        $dataArray [] = [
-//            'currency' => 'EUR',
-//            'start_date' => '2019-03-14',
-//            'end_date' => '2020-03-14',
-//            'city' => 'Berlin',
-//            'city_id' => 55133,
-//            'country_code' => 'DE',
-//        ];
+        $dataArray [] = [
+            'currency' => 'EUR',
+            'start_date' => '2019-03-14',
+            'end_date' => '2020-03-14',
+            'city' => 'Berlin',
+            'city_id' => 55133,
+            'country_code' => 'DE',
+        ];
 
         foreach ($dataArray as $instance) {
 

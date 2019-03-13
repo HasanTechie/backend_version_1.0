55c64b3cd68f09bc1c2e6e721783f0f846a4003a
hasanabbax <hasanabbax@gmail.com>
2019-03-13 10:05:33 +0000

    entered competitors data of hotel emona Aquaeductus

--- database/seeds/tests&basics/EnteringHotelCompetitorsSeeder.php
+++ database/seeds/tests&basics/EnteringHotelCompetitorsSeeder.php
@@ -12,38 +12,92 @@ class EnteringHotelCompetitorsSeeder extends Seeder
     public function run()
     {
         //
-        $hotelUid = '5c80a2d79d162';
-        $hotelName = 'Hotel Emona Aquaeductus';
+//        $hotelUid = '5c80a2d79d162';
+//        $hotelName = 'Hotel Emona Aquaeductus';
+        $hotelUid = '5c878f0d161a5';
+        $hotelName = 'Emona Aquaeductus';
 
 
-        $hotelCompetitorArray[] = [
-            'name' => 'Best Western Plus Hotel Universo',
-            'uid' => '5c80a2de16914'
-        ];
-        $hotelCompetitorArray[] = [
-            'name' => 'Napoleon Hotel',
-            'uid' => '5c80a2e45d1f3'
-        ];
-        $hotelCompetitorArray[] = [
-            'name' => 'Hotel Best Roma',
-            'uid' => '5c80a2ea97a02'
-        ];
-        $hotelCompetitorArray[] = [
-            'name' => 'Hotel Latinum',
-            'uid' => '5c80a2f073e78'
-        ];
-        $hotelCompetitorArray[] = [
-            'name' => 'SHG Hotel Portamaggiore',
-            'uid' => '5c80a2f649c4c'
-        ];
-        $hotelCompetitorArray[] = [
-            'name' => 'Hotel Novecento',
-            'uid' => '5c80a2fc08711'
-        ];
-        $hotelCompetitorArray[] = [
-            'name' => 'Hotel Bled',
-            'uid' => '5c80a3019317b'
-        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Best Western Plus Hotel Universo',
+//            'uid' => '5c80a2de16914',
+//            'hotel_provider' => 'eurobookings.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Napoleon Hotel',
+//            'uid' => '5c80a2e45d1f3',
+//            'hotel_provider' => 'eurobookings.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Hotel Best Roma',
+//            'uid' => '5c80a2ea97a02',
+//            'hotel_provider' => 'eurobookings.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Hotel Latinum',
+//            'uid' => '5c80a2f073e78',
+//            'hotel_provider' => 'eurobookings.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'SHG Hotel Portamaggiore',
+//            'uid' => '5c80a2f649c4c',
+//            'hotel_provider' => 'eurobookings.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Hotel Novecento',
+//            'uid' => '5c80a2fc08711',
+//            'hotel_provider' => 'eurobookings.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Hotel Bled',
+//            'uid' => '5c80a3019317b',
+//            'hotel_provider' => 'eurobookings.com',
+//        ];
+
+//
+//
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Best Western Plus Hotel Universo',
+//            'uid' => '5c878746e093f',
+//            'hotel_provider' => 'hrs.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Napoleon',
+//            'uid' => '5c87892e8bbfe',
+//            'hotel_provider' => 'hrs.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Best Roma',
+//            'uid' => '5c87a92647e8f',
+//            'hotel_provider' => 'hrs.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Portamaggiore Shg Hotel',
+//            'uid' => '5c87b7025409d',
+//            'hotel_provider' => 'hrs.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Novecento',
+//            'uid' => '5c879059a6d60',
+//            'hotel_provider' => 'hrs.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Bled',
+//            'uid' => '5c878a5aa27c3',
+//            'hotel_provider' => 'hrs.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Hotel Milton Roma',
+//            'uid' => '5c878b020bb16',
+//            'hotel_provider' => 'hrs.com',
+//        ];
+//        $hotelCompetitorArray[] = [
+//            'name' => 'Infinito',
+//            'uid' => '5c879e6fc1545',
+//            'hotel_provider' => 'hrs.com',
+//        ];
+
+
 
         $j = 0;
         foreach ($hotelCompetitorArray as $competitorHotel) {
@@ -55,7 +109,7 @@ class EnteringHotelCompetitorsSeeder extends Seeder
                 'hotel_name' => $hotelName,
                 'hotel_competitor_uid' => $competitorHotel['uid'],
                 'hotel_competitor_name' => $competitorHotel['name'],
-                'hotel_provider' => 'eurobookings.com',
+                'hotel_provider' => $competitorHotel['hotel_provider'],
                 'created_at' => DB::raw('now()'),
                 'updated_at' => DB::raw('now()')
             ]);

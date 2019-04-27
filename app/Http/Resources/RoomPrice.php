893f83883269a752d408c638fae8c17d07f708dc
hasanabbax <hasanabbax@gmail.com>
2019-04-27 01:13:32 +0000

    code refactoring

--- app/Http/Resources/RoomPrice.php
+++ app/Http/Resources/RoomPrice.php
@@ -17,23 +17,24 @@ class RoomPrice extends JsonResource
 //        return parent::toArray($request);
         return [
             'room_id' => $this->id,
-//            's_no' => $this->s_no,
-            'price' => round($this->price,2),
-//            'currency' => $this->currency,
-//            'room' => $this->room,
-//            'photo' => $this->photo,
-//            'hotel_uid' => $this->hotel_uid,
-//            'hotel_eurobooking_id' => $this->hotel_eurobooking_id,
+            'price' => $this->price,
+            'room' => $this->room,
             'hotel_name' => $this->hotel_name,
             'hotel_id' => $this->hotel_id,
-//            'number_of_adults_in_room_request' => $this->number_of_adults_in_room_request,
             'check_in_date' => $this->check_in_date,
-//            'check_out_date' => $this->check_out_date,
-//            'rid' => $this->rid,
-//            'request_date' => $this->request_date,
-//            'source' => $this->source,
-//            'created_at' => $this->created_at,
-//            'updated_at' => $this->updated_at,
+
         ];
     }
 }
+
+/*            'currency' => $this->currency,
+            'photo' => $this->photo,
+            'hotel_uid' => $this->hotel_uid,
+            'hotel_eurobooking_id' => $this->hotel_eurobooking_id,
+            'number_of_adults_in_room_request' => $this->number_of_adults_in_room_request,
+            'check_out_date' => $this->check_out_date,
+            'rid' => $this->rid,
+            'request_date' => $this->request_date,
+            'source' => $this->source,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,*/

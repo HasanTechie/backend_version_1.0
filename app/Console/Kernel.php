6fa12c32f069ece017dd4710c652329bf7e3c341
hasanabbax <hasanabbax@gmail.com>
2019-04-30 14:14:19 +0000

    reduced timout

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -48,7 +48,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('command:gatherhrsroomsprices')
             ->withoutOverlapping()
             ->runInBackground()
-            ->dailyAt('15:24')//run once
+            ->dailyAt('15:44')//run once
             ->tuesdays()
             ->appendOutputTo(storage_path('app/mylogs/GatherHrsRoomsPricesCommand' . date("Y-m-d") . '.log'));
 

bb66bd758cd6c924f07beb5f3072fdefe92a9ec1
hasanabbax <hasanabbax@gmail.com>
2019-05-03 11:04:15 +0000

    updated api routes for jwt authentication

--- app/User.php
+++ app/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    
+
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *

14810b89ab73423c42a04637e716dabd0f5eb9e9
hasanabbax <hasanabbax@gmail.com>
2019-05-07 11:47:35 +0000

    code refactoring

--- database/seeds/webscraping/BlazingProxiesSeeder.php
+++ database/seeds/webscraping/BlazingProxiesSeeder.php
@@ -21,7 +21,7 @@ class BlazingProxiesSeeder extends Seeder
 //        $url ='https://www.schoolholidayseurope.eu/austria/';
 //        $url = 'http://www.eurobookings.com/search.html?q=start:2019-04-05;end:2019-04-06;rmcnf:1[2,0];dsti:3023;dstt:1;dsts:Rome;frm:9;sort:0_desc;cur:EUR;stars:0;';
 //        $url = 'https://www.hrs.com/en/hotel/Vienna/d-45883/1#container=&locationId=45883&requestUrl=%2Fen%2Fhotel%2FVienna%2Fd-45883&showAlternates=false&toggle=&arrival=2019-04-14&departure=2019-04-15&lang=en&minPrice=false&roomType=double&singleRoomCount=0&doubleRoomCount=1';
-        $proxy = '207.229.93.66:1029';
+        $proxy = '207.229.93.66:1027';
 
         while (0 == 0) {
             $client = PhantomClient::getInstance();
@@ -31,7 +31,7 @@ class BlazingProxiesSeeder extends Seeder
             $client->getEngine()->addOption("--proxy=http://" . $proxy);
             $client->isLazy(); // Tells the client to wait for all resources before rendering
             $request = $client->getMessageFactory()->createRequest($url);
-            $request->setTimeout(20000);
+//            $request->setTimeout(20000);
             $response = $client->getMessageFactory()->createResponse();
             // Send the request
             $client->send($request, $response);

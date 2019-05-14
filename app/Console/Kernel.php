2874e57983815330d1d98d7e67ee35f408caf8a0
hasanabbax <hasanabbax@gmail.com>
2019-05-14 19:13:31 +0000

    updated kernel console

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,20 +38,18 @@ class Kernel extends ConsoleKernel
 //            ->appendOutputTo(storage_path('app/mylogs/CorrectingSNoCommand.log'));
 
         /*
+        */
         $schedule->command('command:gatherhrshotels')
             ->withoutOverlapping()
             ->runInBackground()
 //            ->onOneServer() //need cache driver, more info at : https://laravel.com/docs/5.7/scheduling
-            ->daily()//run once
-            ->thursdays()//run only on certain day
+            ->weekly()//run once
             ->appendOutputTo(storage_path('app/mylogs/GatherHrsHotelsCommand' . date("Y-m-d") . '.log'));
 
-        */
         $schedule->command('command:gatherhrsroomsprices')
             ->withoutOverlapping()
             ->runInBackground()
             ->dailyAt('05:12')//run once
-            ->mondays()
             ->appendOutputTo(storage_path('app/mylogs/GatherHrsRoomsPricesCommand' . date("Y-m-d") . '.log'));
 
 

88f5a0ee5bc9eccd137311dc6597af1839947686
hasanabbax <hasanabbax@gmail.com>
2019-05-15 13:33:22 +0000

    updated Kernel timings

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -50,7 +50,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('command:gatherhrsroomsprices')
             ->withoutOverlapping()
             ->runInBackground()
-            ->dailyAt('04:45')//run once
+            ->dailyAt('05:45')//run once
             ->appendOutputTo(storage_path('app/mylogs/GatherHrsRoomsPricesCommand' . date("Y-m-d") . '.log'));
 
         /*

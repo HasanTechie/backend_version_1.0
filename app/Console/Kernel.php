7cdd29752b9798d28efdbdf415d173f178b960e1
hasanabbax <hasanabbax@gmail.com>
2019-05-16 15:34:13 +0000

    restarted data gathering using database driver

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -50,7 +50,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('command:gatherhrsroomsprices')
             ->withoutOverlapping()
             ->runInBackground()
-            ->dailyAt('07:15')//run once
+            ->dailyAt('00:14')//run once
             ->appendOutputTo(storage_path('app/mylogs/GatherHrsRoomsPricesCommand' . date("Y-m-d") . '.log'));
 
         /*

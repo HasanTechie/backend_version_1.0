75af7a1441b4185a0ee6782e5647c452d1ac715c
hasanabbax <hasanabbax@gmail.com>
2019-05-20 03:47:04 +0000

    disabled user registration via backend form

--- config/auth.php
+++ config/auth.php
@@ -50,7 +50,7 @@ return [
             'driver' => 'passport',
             'provider' => 'users',
         ],
-        
+
         'adminapi' => [
             'driver' => 'passport',
             'provider' => 'admins',

a11ac00e524cf0e28a4710c715f6dd4b043e7045
hasanabbax <hasanabbax@gmail.com>
2019-05-22 05:43:00 +0000

    changes in Api/AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,85 +2,85 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\User;
-use GuzzleHttp\Client;
-use Illuminate\Http\Request;
+use GuzzleHttp\Client as GuzzleClient;
 use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-
+    //
     public function register(Request $request)
     {
         $request->validate([
-            'email' => 'required',
-            'name' => 'required',
-            'password' => 'required'
+            'name' => 'required|min:3',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'hotel_id' => 'required',
         ]);
 
-        $user = new User();
-        $user->email = $request->email;
+        $user = new User;
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->hotel_id = $request->hotel_id;
+        $user->status = 0;
         $user->password = bcrypt($request->password);
+        $user->hotel_id = $request->hotel_id;
         $user->save();
 
-        $http = new Client;
+        $http = new GuzzleClient;
         $response = $http->post(url('oauth/token'), [
             'form_params' => [
                 'grant_type' => 'password',
-                'client_id' => '2',
-                'client_secret' => '5HMCDi6OBKppKXWQqYwg5WHe8ahbqYYlKHtoP1y4',
+                'client_id' => env('OAUTH_GRANT_SECRET_ID'),
+                'client_secret' => env('OAUTH_GRANT_SECRET_KEY'),
                 'username' => $request->email,
                 'password' => $request->password,
                 'scope' => '',
             ],
         ]);
-        return response(['auth' => json_decode((string)$response->getBody(), true), 'user' => $user]);
-
+        return response(['data' => json_decode((string)$response->getBody(), true)]);
     }
 
     public function login(Request $request)
     {
-
         $request->validate([
-            'email' => 'required',
-            'password' => 'required'
+            'email' => 'required|email',
+            'password' => 'required|min:6',
         ]);
         $user = User::where('email', $request->email)->first();
+
         if (!$user) {
             return response(['status' => 'error', 'message' => 'User not found']);
         }
+
         if (Hash::check($request->password, $user->password)) {
-            $http = new Client;
+            $http = new GuzzleClient;
             $response = $http->post(url('oauth/token'), [
                 'form_params' => [
                     'grant_type' => 'password',
-                    'client_id' => '2',
-                    'client_secret' => '5HMCDi6OBKppKXWQqYwg5WHe8ahbqYYlKHtoP1y4',
+                    'client_id' => env('OAUTH_GRANT_SECRET_ID'),
+                    'client_secret' => env('OAUTH_GRANT_SECRET_KEY'),
                     'username' => $request->email,
                     'password' => $request->password,
                     'scope' => '',
                 ],
             ]);
-            return response(['auth' => json_decode((string)$response->getBody(), true), 'user' => $user]);
-
-        } else {
-            return response(['message' => 'password not match', 'status' => 'error']);
+            return response(['data' => json_decode((string)$response->getBody(), true)]);
         }
     }
 
+
     public function refreshToken()
     {
-        $http = new Client;
+        $http = new GuzzleClient;
         $response = $http->post(url('oauth/token'), [
             'form_params' => [
                 'grant_type' => 'refresh_token',
                 'refresh_token' => request('refresh_token'),
-                'client_id' => '2',
-                'client_secret' => '5HMCDi6OBKppKXWQqYwg5WHe8ahbqYYlKHtoP1y4',
+                'client_id' => env('OAUTH_GRANT_SECRET_ID'),
+                'client_secret' => env('OAUTH_GRANT_SECRET_KEY'),
                 'scope' => '',
             ],
         ]);

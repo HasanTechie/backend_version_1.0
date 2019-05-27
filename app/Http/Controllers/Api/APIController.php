eb4291c601f6843536b0cc7d25a016b16bf97a3c
hasanabbax <hasanabbax@gmail.com>
2019-05-27 15:42:03 +0000

    fix 842

--- app/Http/Controllers/Api/APIController.php
+++ app/Http/Controllers/Api/APIController.php
@@ -219,7 +219,7 @@ class APIController extends Controller
     public function HRSHotelsCompetitorsRoomsPrices($rows, $apiKey, $hotel, $dateFrom, $dateTo, $competitorIds)
     {
         if ($apiKey == $this->apiKey) {
-            $competitorIdsArray = explode(',', str_replace(array('[', ']'), '', $competitorIds));
+            $competitorIdsArray = explode(',', $competitorIds);
             $dates = DB::table('rooms_hrs')
                 ->select(DB::raw('prices_hrs.check_in_date'))
                 ->join('prices_hrs', 'prices_hrs.room_id', '=', 'rooms_hrs.id')
@@ -243,10 +243,8 @@ class APIController extends Controller
 //                        ['request_date', '<=', date("Y-m-d")],
 //                        ['request_date', '>=', date("Y-m-d", strtotime("-5 day"))],
                     ])->groupBy('room', 'criteria', 'check_in_date', 'room_type', 'price')->get();
-            }
-            if (isset($mainHotelRooms)) {
-                foreach ($mainHotelRooms as $mainHotelRoom) {
-                    foreach ($dates as $date) {
+                if (isset($mainHotelRooms)) {
+                    foreach ($mainHotelRooms as $mainHotelRoom) {
                         foreach ($competitorIdsArray as $competitorId) {
                             $competitorsRooms = DB::table('rooms_hrs')
                                 ->select(DB::raw('hotels_hrs.name as hotel_name, hotels_hrs.id as hotel_id, criteria, rooms_hrs.room, prices_hrs.price, prices_hrs.request_date'))
@@ -278,12 +276,12 @@ class APIController extends Controller
                                 }
                             }
                         }
+                        $mainHotelRoom->competitors = $dA2;
+                        $dA2 = null;
                     }
+                    return CompetitorRoomPriceResource::collection($mainHotelRooms);
                 }
-                $mainHotelRoom->competitors = $dA2;
-                $dA2 = null;
             }
-            return CompetitorRoomPriceResource::collection($mainHotelRooms);
 
 //            dd('Error: Data Not Found : HRSHotelsCompetitorsRoomsPrices');
         } else {

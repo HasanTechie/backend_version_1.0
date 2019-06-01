2a07da5478993bf4b706e457ee79aad180e3f8c6
hasanabbax <hasanabbax@gmail.com>
2019-06-01 12:18:56 +0000

    updated api

--- app/Http/Controllers/Api/APIController.php
+++ app/Http/Controllers/Api/APIController.php
@@ -115,11 +115,12 @@ class APIController extends Controller
         }
     }
 
-    public function HRSHotelsCompetitorsPricesApex($rows, $apiKey, $userId, $dateFrom, $dateTo, $competitorIds, $room)
+    public function HRSHotelsCompetitorsPricesApex($rows, $apiKey, $hotelId, $dateFrom, $dateTo, $competitorIds, $room)
     {
 
-        dd('reached0');
-        $hotelId=3045;
+        dd(auth()->user()->getAuthIdentifier());
+
+
         if ($apiKey == $this->apiKey) {
             $competitorIdsArray = explode(',', $competitorIds);
 

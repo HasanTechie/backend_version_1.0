dcc58683afd11a4e80dc353978b4c517bf71324f
hasanabbax <hasanabbax@gmail.com>
2019-06-06 12:23:22 +0000

    final commit before deployment

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      * //     */
     public function run()
     {
-//        $this->call('PracticeSeeder');
-        $this->call('TransferDataFromPricesTableSeeder');
+        $this->call('PracticeSeeder');
+//        $this->call('TransferDataFromPricesTableSeeder');
 //        $this->call('ConvertingUIDsOfTables');
 //        $this->call('GatheringPublicHolidaysDataSeeder');
 

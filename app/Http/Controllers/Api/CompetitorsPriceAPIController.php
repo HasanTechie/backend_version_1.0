afd0bbef29acf1723992fc17b967f5df1146334d
hasanabbax <hasanabbax@gmail.com>
2019-06-10 17:18:35 +0000

    added CompetitorsPrice as separate controller

---
+++ app/Http/Controllers/Api/CompetitorsPriceAPIController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CompetitorAvgPrice as CompetitorAvgPriceResource;
+use App\Http\Resources\CompetitorPriceApex as CompetitorPriceResourceApex;
+use App\Http\Resources\CompetitorRoomPrice as CompetitorRoomPriceResource;
+use App\Http\Resources\CompetitorRoomAvgPrice as CompetitorRoomAvgPriceResource;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Http\Request;
+
+class CompetitorsPriceAPIController extends Controller
+{
+    //
+}

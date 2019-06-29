8f535024d3ca6b9305fb1087d97034c985015dbf
hasanabbax <hasanabbax@gmail.com>
2019-06-29 11:11:10 +0000

    added migration

---
+++ database/migrations/2019_06_28_225148_create_hotel_ids_for_data_gathering_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHotelIdsForDataGatheringTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotel_ids_for_data_gathering', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->unique();
+            $table->unsignedBigInteger('hotel_id');
+            $table->string('city');
+            $table->string('country_code');
+            $table->string('source');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotel_ids_for_data_gathering');
+    }
+}

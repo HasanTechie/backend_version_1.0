25306af6c4891bb3e91b29560cd236c93aec5a91
hasanabbax <hasanabbax@gmail.com>
2019-07-02 01:00:51 +0000

    practicing hotel hunder web scraping

--- database/seeds/webscraping/ProxyCrawlSeeder.php
+++ database/seeds/webscraping/ProxyCrawlSeeder.php
@@ -22,7 +22,7 @@ class ProxyCrawlSeeder extends Seeder
     {
         //
         $url = 'https://api.myip.com/';
-        $url = 'https://www.hotelhunter.com/Hotel/Search?fileName=H10_Berlin_KuDamm&destination=place:Berlin&radius=0mi&checkin=2019-07-13&checkout=2019-07-14&Rooms=1&adults_1=2&returnPath=%2FHotels%2FSearch%3Fdestination%3Dplace%3ABerlin%26radius%3D0mi%26checkin%3D2019-07-13%26checkout%3D2019-07-14%26Rooms%3D1%26adults_1%3D2%26pageSize%3D15%26pageIndex%3D0%26sort%3DPopularity-desc%26showSoldOut%3Dfalse%26scroll%3D1431%26HotelID%3D%26mapState%3Dexpanded%253D0';
+        $url = 'https://www.hotelhunter.com/Hotels/Search?destination=place%3ABerlin&checkin=2019-07-13&checkout=2019-07-14&Rooms=1&adults_1=2&languageCode=EN&currencyCode=EUR#destination=place:Berlin&radius=0mi&checkin=2019-07-13&checkout=2019-07-14&Rooms=1&adults_1=2&pageSize=15&pageIndex=0&sort=Popularity-desc&showSoldOut=false&scroll=1431&HotelID=&mapState=expanded%3D0';
 //        $url = 'https://publicholidays.de/baden-wurttemberg/2019-dates/';
 //        $url ='https://www.schoolholidayseurope.eu/austria/';
 //        $url = 'http://www.eurobookings.com/search.html?q=start:2019-04-05;end:2019-04-06;rmcnf:1[2,0];dsti:3023;dstt:1;dsts:Rome;frm:9;sort:0_desc;cur:EUR;stars:0;';
@@ -49,8 +49,8 @@ class ProxyCrawlSeeder extends Seeder
         while (0 == 0) {
             $client = PhantomClient::getInstance();
             $client->getEngine()->setPath(base_path() . '/bin/phantomjs');
-//            $client->getEngine()->addOption('--load-images=false');
-//            $client->getEngine()->addOption('--ignore-ssl-errors=true');
+            $client->getEngine()->addOption('--load-images=false');
+            $client->getEngine()->addOption('--ignore-ssl-errors=true');
 //            $client->getEngine()->addOption("--proxy=http://" . $proxy);
             $client->isLazy(); // Tells the client to wait for all resources before rendering
             $request = $client->getMessageFactory()->createRequest($url);
@@ -60,8 +60,15 @@ class ProxyCrawlSeeder extends Seeder
             $client->send($request, $response);
             $crawler = new Crawler($response->getContent());
 
-            if ($crawler->count()) {
-                echo $crawler->html();
+            if ($crawler->filter('body')->count()) {
+                $crawler->filter('td')->each(function ($node) {
+                    echo $node->text() . "\n";
+                });
+
+                echo $crawler->filter('div#SearchResultsBody')->text();
+
+                Storage::put('hotelhunder.html', $crawler->html());
+                dd('reached');
 //                echo $crawler->text() . ' ' . Carbon::now()->toDateTimeString() . "\n";
             } else {
                 echo 'empty : ' . $response->getStatus() . "\n";

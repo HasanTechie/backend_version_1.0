8c06d03f52e763979277a3f6f86c63d93480265f
hasanabbax <hasanabbax@gmail.com>
2019-07-22 20:19:33 +0000

    added CSVs methods

--- app/Http/Controllers/Api/FilesController.php
+++ app/Http/Controllers/Api/FilesController.php
@@ -49,6 +49,15 @@ class FilesController extends Controller
             $fileURL = Storage::url($file);
             $fileType = pathinfo($file)['extension'];
             $userID = $request->user()->id;
+
+            DB::table('files')->insert([
+                'user_id' => $userID,
+                'file_name' => $fileName,
+                'file_url' => $fileURL,
+                'file_type' => $fileType,
+                'created_at' => DB::raw('now()'),
+                'updated_at' => DB::raw('now()')
+            ]);
         }
 
         return response(['status' => 'success'], 200);
